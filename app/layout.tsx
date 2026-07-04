f8ff199ab8aa5e1c336d7f2be568d9786150da15
AnjaLLehmann <lehmann_anjalouise@hotmail.com>
2026-07-04 23:03:14 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Fraunces, Inter, MonteCarlo } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -54,6 +55,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-0PBKGCEWE5" />
       </body>
     </html>
   );
